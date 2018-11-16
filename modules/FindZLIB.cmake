c9d033eec44d1b73251cdad182c41cd40ca1568c
Tomáš Malý <tomas.maly@melown.com>
2018-11-16 16:34:44 +0100

    fix zlib

--- modules/FindZLIB.cmake
+++ modules/FindZLIB.cmake
@@ -1,6 +1,23 @@
 
-set(Zlib_LIBRARIES $<TARGET_LINKER_FILE:zlibstatic>)
-get_target_property(Zlib_INCLUDE_DIRS zlibstatic INTERFACE_INCLUDE_DIRECTORIES)
+if(NOT TARGET zlibstatic)
+    message(FATAL "zlibstatic NOT FOUND")
+endif()
+
+if(NOT TARGET ZLIB::ZLIB)
+    set(lib $<TARGET_LINKER_FILE:zlibstatic>)
+    get_target_property(inc zlibstatic INTERFACE_INCLUDE_DIRECTORIES)
+
+    add_library(ZLIB_ZLIB INTERFACE IMPORTED GLOBAL)
+    set_target_properties(ZLIB_ZLIB PROPERTIES
+        INTERFACE_LINK_LIBRARIES "${lib}"
+        INTERFACE_INCLUDE_DIRECTORIES "${inc}"
+    )
+    add_dependencies(ZLIB_ZLIB zlibstatic)
+    add_library(ZLIB::ZLIB ALIAS ZLIB_ZLIB)
+endif()
+
+get_target_property(Zlib_LIBRARIES ZLIB::ZLIB INTERFACE_LINK_LIBRARIES)
+get_target_property(Zlib_INCLUDE_DIRS ZLIB::ZLIB INTERFACE_INCLUDE_DIRECTORIES)
 set(Zlib_LIBRARY ${Zlib_LIBRARIES})
 set(Zlib_INCLUDE_DIR ${Zlib_INCLUDE_DIRS})
 set(Zlib_FOUND TRUE)
@@ -10,7 +27,4 @@ set(ZLIB_INCLUDE_DIRS ${Zlib_INCLUDE_DIRS})
 set(ZLIB_LIBRARY ${Zlib_LIBRARIES})
 set(ZLIB_INCLUDE_DIR ${Zlib_INCLUDE_DIRS})
 set(ZLIB_FOUND TRUE)
-
-
-add_library(ZLIB::ZLIB ALIAS zlibstatic)
 
