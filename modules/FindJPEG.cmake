6c0c8468f28bbf115300d64f8caf89bb99d98654
Tomas Maly <malytomas@ucpu.cz>
2021-04-01 12:03:12 +0200

    update 3rd-party dependencies (except proj and sdl)

--- modules/FindJPEG.cmake
+++ modules/FindJPEG.cmake
@@ -1,12 +1,12 @@
 
-if(NOT TARGET jpeg-static)
-    message(FATAL "jpeg-static NOT FOUND")
+if(NOT TARGET vts-jpeg)
+    message(FATAL "vts-jpeg NOT FOUND")
 endif()
 
-set(Jpeg_LIBRARIES jpeg-static)
-get_target_property(Jpeg_INCLUDE_DIRS jpeg-static INTERFACE_INCLUDE_DIRECTORIES)
-set(Jpeg_LIBRARY ${Jpeg_LIBRARIES})
-set(Jpeg_INCLUDE_DIR ${Jpeg_INCLUDE_DIRS})
+set(Jpeg_LIBRARIES vts-jpeg)
+get_target_property(Jpeg_INCLUDE_DIRS vts-jpeg INTERFACE_INCLUDE_DIRECTORIES)
+set(Jpeg_LIBRARY ${Freetype_LIBRARIES})
+set(Jpeg_INCLUDE_DIR ${Freetype_INCLUDE_DIRS})
 set(Jpeg_FOUND TRUE)
 
 set(JPEG_LIBRARIES ${Jpeg_LIBRARIES})
